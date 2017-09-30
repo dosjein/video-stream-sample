7f1e5e5128147d36fa9667460d64f6aa2a8d0400
Daddy <daddy@family.dev>
2017-09-30 14:18:28 +0300

    added other details

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get('/video', function(req, res) {
   }
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!')
+app.listen(3145, function () {
+  console.log('Listening on port 3145!')
 })
